7175789788d490a0d60c019d46a85ccc6798e286
goodBoyMusa <114241733+goodBoyMusa@users.noreply.github.com>
2023-04-11 02:35:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("hello world")
+import numpy as np
+
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Convert the list to a numpy array
+arr = np.array(numbers)
+
+# Use numpy to calculate the sum, mean, and standard deviation of the array
+total = np.sum(arr)
+average = np.mean(arr)
+std_dev = np.std(arr)
+
+# Print the results to the console
+print("The sum of the numbers is:", total)
+print("The average of the numbers is:", average)
+print("The standard deviation of the numbers is:", std_dev)
